bdc033b6484785705dd2698f04d6cacf3ab62067
Lester Cheong <lestercheong70+github@outlook.com>
2024-09-22 02:58:57 +0800

    feat: menu alters based on device

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,6 +6,7 @@ module.exports = {
       2,
       'always',
       [
+        'content',
         'docs',
         'chore',
         'build',
--- src/components/Navigation/SideNavigation/SideNavigation.tsx
+++ src/components/Navigation/SideNavigation/SideNavigation.tsx
@@ -1,5 +1,7 @@
 import { Checkbox, Icon, Tooltip } from "@blueprintjs/core";
 import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { routes } from "../../../router/router";
 import {
   assignCollapseState,
   toggleKeepMenuOpenState,
@@ -8,10 +10,12 @@ import {
   selectIsSideNavigationCollapsed,
   selectKeepMenuOpen,
 } from "../../../store/selectors/sidebarSelector";
-import { routes } from "../../../router/router";
-import { Link, useLocation } from "react-router-dom";
+import { isMobileDevice } from "../../../helper";
 
 const SideNavigation = () => {
+  const ICON_SIZE = isMobileDevice() ? 22 : 16;
+
+  const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch();
   const isSideNavigationCollapsed = useSelector(selectIsSideNavigationCollapsed);
@@ -21,8 +25,9 @@ const SideNavigation = () => {
     return routes.map((route) => {
       return (
         <Link
-          className={`text-white transition-all cursor-pointer p-2 m-1 mr-0 pr-0 outline-1 ${location.pathname == route.routeObject.path ? "text-blue-800 font-bold bg-white" : ""}
-           hover:outline-dashed hover:bg-none hover:font-bold 
+          className={`transition-all cursor-pointer p-3 m-1 mr-0 pr-0 outline-1 rounded-l-lg
+          hover:outline-dashed hover:bg-blue-900 hover:text-blue-100
+           ${location.pathname == route.routeObject.path ? "font-bold bg-white text-blue-900 text-3xl md:text-xl my-4 ml-4" : "text-white"}
           `}
           to={route.routeObject.path as string}
           key={route.title}
@@ -37,36 +42,50 @@ const SideNavigation = () => {
   };
 
   return (
-    <>
+    <div className="font-kalam text-xl sm:text-base h-fit">
       <Tooltip
-        className="p-3 my-auto"
+        className="p-4 md:p-3"
         content={`${isSideNavigationCollapsed ? "Expand Menu" : "Collapse Menu"}`}
       >
         <Icon
-          size={16}
-          className="my-auto"
+          size={ICON_SIZE}
           icon={`${isSideNavigationCollapsed ? "chevron-right" : "chevron-left"}`}
+          className="my-auto"
           onClick={() => {
             if (keepMenuOpen) dispatch(toggleKeepMenuOpenState());
             dispatch(assignCollapseState(true));
           }}
         />
       </Tooltip>
-      <div
-        className={`mx-4 mr-0 animate-[fade-in] ${isSideNavigationCollapsed ? "hidden" : "block"}`}
-      >
-        <Checkbox
-          className="mt-4"
-          label="keep menu opened"
-          checked={keepMenuOpen}
+      <div className={`mt-5 flex ${isSideNavigationCollapsed ? "visible" : "hidden"}`}>
+        {/* TODO: feature for collapsed menu icons? */}
+        <Icon
+          size={ICON_SIZE}
+          icon="home"
+          className="m-auto transition-all hover:scale-125 hover:text-blue-300"
           onClick={() => {
-            dispatch(toggleKeepMenuOpenState());
+            dispatch(assignCollapseState(false));
+            navigate(routes[0].routeObject.path as string);
           }}
         />
-        <hr />
+      </div>
+      <div className={`mx-4 mr-0 ${isSideNavigationCollapsed ? "hidden" : "visible"}`}>
+        {isMobileDevice() ? null : (
+          <>
+            <Checkbox
+              className="mt-4 hidden md:visible"
+              label="keep menu opened"
+              checked={keepMenuOpen}
+              onClick={() => {
+                dispatch(toggleKeepMenuOpenState());
+              }}
+            />
+            <hr />
+          </>
+        )}
         <div className="mt-4 flex flex-col truncate">{LinksFromRouter()}</div>
       </div>
-    </>
+    </div>
   );
 };
 
